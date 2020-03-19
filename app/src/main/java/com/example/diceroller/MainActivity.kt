9c4959f329f12154e8a7841f6c8e62f3b947739c
elisha <ebehelisha@gmail.com>
2020-03-19 07:25:20 +0000

    gradle updated to androidx

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.diceroller
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage =  findViewById(R.id.dice_image)
 
         var rollButton: Button = findViewById(R.id.roll_button);
         rollButton.text = "Let's Roll"
@@ -25,7 +31,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val diceImage: ImageView =  findViewById(R.id.dice_image)
         val drawableResource = when(randomInt){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
